bc908119e340fc979224269b723430f6f2d53fdd
louiswijaya <60252208+louiswijaya@users.noreply.github.com>
2020-01-28 20:52:20 +0700

    Upload index.html instead

---
+++ koneksi.php
@@ -0,0 +1,7 @@
+<?php
+$servername     = "localhost";
+$username       = "id12115544_gis";
+$password       = "louis007";
+$db   			= "id12115544_gis";
+$conn           = mysqli_connect($servername, $username, $password, $db);
+?>
